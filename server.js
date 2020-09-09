c3f9706b28a5db70de3f8e857cf04d189efd7545
Opilane TTHK <opilane@tthk.ee>
2020-09-09 10:54:57 +0300

    Add villains

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 let superheroes=require("superheroes");
-console.log(superheroes.all);
-console.log(superheroes.random());
+/*console.log(superheroes.all);
+console.log(superheroes.random());*/
+let supervillains=require("supervillains");
+/*console.log(supervillains.all);*/
+/*dostaju vse imena*/
+supervillains.all.forEach(element => {
+    console.log(element)
+});
+/*name random*/ 
+console.log(supervillains.random());
+/*3 zadanie */
+console.log(`${superheroes.random()} fights ${supervillains.random()}`);
+/*console.log(supervillains.random());*/
+/*task1 get the names of all supervillains
+2-get a random supervillain name
+3-make a random superhero fight a random supervilliain in the console.log() */
